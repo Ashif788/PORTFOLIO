12c49d93ca0ac321436991f2266ab35bffd15915
unknown <ashigasif@gmail.com>
2025-01-29 11:49:05 -0800

    first commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -87,16 +87,7 @@ function Home2() {
                   <AiFillGithub />
                 </a>
               </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/ashif12"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
+              
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/ashif4419/"
